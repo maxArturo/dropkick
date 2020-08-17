cba419f3e9d2191c603aa2f09315e595867b0d2e
Max Alcala <maxarturo@gmail.com>
2020-08-17 07:29:17 -0500

    feat: server working

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-() => {
-  return true;
-};
+import fastify from 'fastify';
+
+const app = fastify({ logger: true });
+
+app.get('/', (_res, res) => {
+  res.send({ hello: 'world' });
+});
+
+app.listen(3000, (err, address) => {
+  if (err) {
+    throw err;
+  }
+  app.log.info(`server listening on ${address}`);
+});
